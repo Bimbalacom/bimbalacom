8c7fc2dd006e44a1627e97ed6894c8fef9f2c6f6
mrgkanev <contact@mrgkanev.eu>
2020-08-04 18:32:23 +0300

    Updating footer links

--- components/footer.js
+++ components/footer.js
@@ -4,78 +4,87 @@ export default function Footer(){
         <div className="container">
           <div className="row">
             <div className="col-xl-auto mr-xl-5 col-md-3 mb-4 mb-md-0">
-              <h5>Demos</h5>
+              <h5>About us</h5>
               <ul className="nav flex-row flex-md-column">
                 <li className="nav-item mr-3 mr-md-0">
-                  <a href="landing-1.html" className="nav-link fade-page px-0 py-2">Landing 1</a>
+                  <a href="#" className="nav-link fade-page px-0 py-2">Demo</a>
                 </li>
                 <li className="nav-item mr-3 mr-md-0">
-                  <a href="landing-2.html" className="nav-link fade-page px-0 py-2">Landing 2</a>
+                  <a href="#" className="nav-link fade-page px-0 py-2">Pricing</a>
                 </li>
                 <li className="nav-item mr-3 mr-md-0">
-                  <a href="landing-3.html" className="nav-link fade-page px-0 py-2">Landing 3</a>
+                  <a href="#" className="nav-link fade-page px-0 py-2">Features</a>
                 </li>
                 <li className="nav-item mr-3 mr-md-0">
-                  <a href="landing-4.html" className="nav-link fade-page px-0 py-2">Landing 4</a>
+                  <a href="#" className="nav-link fade-page px-0 py-2">Integration</a>
                 </li>
                 <li className="nav-item mr-3 mr-md-0">
-                  <a href="landing-5.html" className="nav-link fade-page px-0 py-2">Landing 5</a>
+                  <a href="#" className="nav-link fade-page px-0 py-2">Why us?</a>
                 </li>
                 <li className="nav-item mr-3 mr-md-0">
-                  <a href="landing-6.html" className="nav-link fade-page px-0 py-2">Landing 6</a>
+                  <a href="#" className="nav-link fade-page px-0 py-2"></a>
                 </li>
               </ul>
             </div>
             <div className="col-xl-auto mr-xl-5 col-md-3">
-              <h5>Pages</h5>
+              <h5>Information</h5>
               <ul className="nav flex-row flex-md-column">
                 <li className="nav-item mr-3 mr-md-0">
-                  <a href="company-about-1.html" className="nav-link fade-page px-0 py-2">Company</a>
+                  <a href="#" className="nav-link fade-page px-0 py-2">Success Stories</a>
                 </li>
                 <li className="nav-item mr-3 mr-md-0">
-                  <a href="blog-listing-1.html" className="nav-link fade-page px-0 py-2">Blog</a>
+                  <a href="#" className="nav-link fade-page px-0 py-2">Customer Reviews</a>
                 </li>
                 <li className="nav-item mr-3 mr-md-0">
-                  <a href="help-center-home.html" className="nav-link fade-page px-0 py-2">Help Center</a>
+                  <a href="/blog/" className="nav-link fade-page px-0 py-2">Blog</a>
                 </li>
                 <li className="nav-item mr-3 mr-md-0">
-                  <a href="careers-1.html" className="nav-link fade-page px-0 py-2">Careers</a>
+                  <a href="#" className="nav-link fade-page px-0 py-2">Solutions</a>
                 </li>
                 <li className="nav-item mr-3 mr-md-0">
-                  <a href="case-studies.html" className="nav-link fade-page px-0 py-2">Case Studies</a>
+                  <a href="#" className="nav-link fade-page px-0 py-2">Partners</a>
                 </li>
                 <li className="nav-item mr-3 mr-md-0">
-                  <a href="pricing-plans.html" className="nav-link fade-page px-0 py-2">Pricing</a>
+                  <a href="#" className="nav-link fade-page px-0 py-2">Affiliates</a>
                 </li>
               </ul>
             </div>
             <div className="col mt-4 mt-md-0 mt-lg-5 mt-xl-0 order-lg-4 order-xl-3">
-              <h5>Articles</h5>
-              <ul className="list-unstyled d-flex flex-wrap">
-                <li className="col-12 col-lg-6 col-xl-12 px-0">
-                  <div className="row my-2 my-md-3">
-                    <a className="col-5" href="#">
-                      <img className="rounded img-fluid hover-fade-out" src="assets/img/blog/thumb-2.jpg"
-                           alt="blog.1.image"/>
-                    </a>
-                    <div className="col">
-                      <a className="h6" href="#">Unveiling our new vision for Jumpstart</a>
-                      <div className="text-small text-muted mt-2">October 10th</div>
-                    </div>
-                  </div>
+            <h5>About Us</h5>
+              <ul className="nav flex-row flex-md-column">
+                <li className="nav-item mr-3 mr-md-0">
+                  <a href="#" className="nav-link fade-page px-0 py-2">Company</a>
                 </li>
-                <li className="col-12 col-lg-6 col-xl-12 px-0">
-                  <div className="row my-2 my-md-3">
-                    <a className="col-5" href="#">
-                      <img className="rounded img-fluid hover-fade-out" src="assets/img/blog/thumb-1.jpg"
-                           alt="blog.2.image"/>
-                    </a>
-                    <div className="col">
-                      <a className="h6" href="#">Making the most of team-building sessions</a>
-                      <div className="text-small text-muted mt-2">October 2nd</div>
-                    </div>
-                  </div>
+                <li className="nav-item mr-3 mr-md-0">
+                  <a href="#" className="nav-link fade-page px-0 py-2">Team</a>
                 </li>
+                <li className="nav-item mr-3 mr-md-0">
+                  <a href="#" className="nav-link fade-page px-0 py-2">Help Center</a>
+                </li>
+                <li className="nav-item mr-3 mr-md-0">
+                  <a href="#" className="nav-link fade-page px-0 py-2">Careers</a>
+                </li>
+                <li className="nav-item mr-3 mr-md-0">
+                  <a href="#" className="nav-link fade-page px-0 py-2">Case Studies</a>
+                </li>
+              </ul>
+            </div>
+            <div className="col mt-4 mt-md-0 mt-lg-5 mt-xl-0 order-lg-4 order-xl-3">
+            <h5>Social</h5>
+              <ul className="nav flex-row flex-md-column">
+                <li className="nav-item mr-3 mr-md-0">
+                  <a href="https://fb.com/bimbalacom" className="nav-link fade-page px-0 py-2">Facebook</a>
+                </li>
+                <li className="nav-item mr-3 mr-md-0">
+                  <a href="https://twitter.com/bimbalacom" className="nav-link fade-page px-0 py-2">Twitter</a>
+                </li>
+                <li className="nav-item mr-3 mr-md-0">
+                  <a href="https://github.com/bimbalacom" className="nav-link fade-page px-0 py-2">Github</a>
+                </li>
+                <li className="nav-item mr-3 mr-md-0">
+                  <a href="#" className="nav-link fade-page px-0 py-2">Youtube</a>
+                </li>
+
               </ul>
             </div>
             <div className="col-lg mt-2 mt-md-5 mt-lg-0 order-lg-3 order-xl-4">
@@ -113,9 +122,9 @@ export default function Footer(){
               className="row flex-column flex-lg-row align-items-center justify-content-center justify-content-lg-between text-center text-lg-left">
             <div className="col-auto">
               <div className="d-flex flex-column flex-sm-row align-items-center text-small">
-                <div className="text-muted">&copy; 2020 Page protected by reCAPTCHA and subject to Google's <a
-                    href="https://www.google.com/policies/privacy/" target="_blank">Privacy Policy</a> and <a
-                    href="https://policies.google.com/terms" target="_blank">Terms of Service</a>
+                <div className="text-muted">&copy; 2020 Bimbala Ltd <a
+                    href="privacy-policy/">Privacy Policy</a> and <a
+                    href="terms-of-service/">Terms of Service</a>
                 </div>
               </div>
             </div>
