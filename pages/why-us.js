39bf54edbd6d9cc7a1d5f9e1bb10c9e1f033df93
Lyuboslav Alexandrov <alexandrovcorp@yahoo.com>
2020-08-10 20:55:11 +0300

    Fix warning and build error

---
+++ pages/why-us.js
@@ -0,0 +1,10 @@
+import Layout from "../components/layout";
+import React from "react";
+
+export default function WhyUS(){
+    return (
+        <Layout title={'Why us?'}>
+            ''
+        </Layout>
+    )
+}
