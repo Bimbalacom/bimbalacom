d9fab409bd18a7dc0867a27a93becf99d30596fc
mrgkanev <contact@mrgkanev.eu>
2020-08-20 00:05:55 +0300

    Feature page

--- pages/features.js
+++ pages/features.js
@@ -1,12 +1,101 @@
 import Link from 'next/link'
 import Layout from "../components/layout";
-import ComingSoon from "../components/comingSoon";
 import React from "react";
+import take_flight from "../components/take_flight";
 
 export default function FirstPost() {
-    return (
-        <Layout title={'Features - Bimbala.com'}>
-        <ComingSoon/>
+  return (
+    <Layout title={'Features - Bimbala.com'}>
+      <take_flight/>
+    <section class="o-hidden">
+        <div class="container">
+          <div class="row align-items-center justify-content-around text-center text-lg-left">
+            <div class="col-md-9 col-lg-6 col-xl-5 mb-4 mb-md-5 mb-lg-0 pl-lg-5 pl-xl-0">
+              <div>
+                <div class="alert bg-secondary rounded-lg d-inline-block mb-4">
+                  <div class="d-flex align-items-center">
+                    <div class="badge badge-pill badge-success">New</div>
+                    <div class="mx-3">Announce a great new feature</div>
+                  </div>
+                </div>
+                <h3 class="h1">Everything you’ll need to launch your site</h3>
+                <p class="lead">Ned ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa.</p>
+                <a href="#" class="lead">Explore More</a>
+              </div>
+            </div>
+            <div class="col-md-9 col-lg-6 col-xl-5">
+              <img src="img/illustrations/illustration-2.svg" alt="Image" class="img-fluid" />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="o-hidden pt-0">
+        <div class="container">
+          <div class="row align-items-center justify-content-around text-center text-lg-left">
+            <div class="col-md-9 col-lg-6 col-xl-5 mb-4 mb-md-5 mb-lg-0 order-lg-2 pl-lg-5 pl-xl-0">
+              <div>
+                <h2 class="h1">Save tons on design and development</h2>
+                <p class="lead">Berspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+              </div>
+              <div class="d-flex flex-wrap justify-content-center justify-content-lg-start">
+                <div class="mb-3 mr-4 ml-lg-0 mr-lg-4" data-aos="fade-left" data-aos-delay="100">
+                  <div class="d-flex align-items-center">
+                    <div class="rounded-circle bg-success-alt">
+                      <img src="img/icons/interface/icon-check.svg" alt="Binoculars icon" class="m-2 icon icon-xs bg-success" />
+                    </div>
+                    <h6 class="mb-0 ml-3">Fully Responsive</h6>
+                  </div>
+                </div>
+                <div class="mb-3 mr-4 ml-lg-0 mr-lg-4" data-aos="fade-left" data-aos-delay="200">
+                  <div class="d-flex align-items-center">
+                    <div class="rounded-circle bg-success-alt">
+                      <img src="img/icons/interface/icon-check.svg" alt="Layouts icon" class="m-2 icon icon-xs bg-success" />
+                    </div>
+                    <h6 class="mb-0 ml-3">Multiple Layouts</h6>
+                  </div>
+                </div>
+                <div class="mb-3 mr-4 ml-lg-0 mr-lg-4" data-aos="fade-left" data-aos-delay="300">
+                  <div class="d-flex align-items-center">
+                    <div class="rounded-circle bg-success-alt">
+                      <img src="img/icons/interface/icon-check.svg" alt="Box icon" class="m-2 icon icon-xs bg-success" />
+                    </div>
+                    <h6 class="mb-0 ml-3">Modular Components</h6>
+                  </div>
+                </div>
+                <div class="mb-3 mr-4 ml-lg-0 mr-lg-4" data-aos="fade-left" data-aos-delay="400">
+                  <div class="d-flex align-items-center">
+                    <div class="rounded-circle bg-success-alt">
+                      <img src="img/icons/interface/icon-check.svg" alt="Lightning icon" class="m-2 icon icon-xs bg-success" />
+                    </div>
+                    <h6 class="mb-0 ml-3">Suits Your Style</h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-9 col-lg-6 col-xl-5 order-lg-1">
+              <img src="img/illustrations/illustration-3.svg" alt="Image" class="img-fluid" />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="bg-primary-3 text-white jarallax pb-0" data-jarallax data-speed="0.2">
+        <img src="img/heros/hero-1.jpg" alt="Image" class="jarallax-img opacity-50" />
+        <div class="container pb-5">
+          <div class="row">
+            <div class="col-xl-5 col-lg-6 col-md-7">
+              <blockquote class="blockquote p-0 border-0 text-white">
+                &ldquo;We all know the stigma around build times and the ever expanding arsenal of tooling in modern web apps. Fear not, Jumpstart does away with all of that.&rdquo;
+                </blockquote>
+              <img class="bg-white opacity-50 mt-3 mt-md-4 mb-3" src="img/logos/brand/kanba.svg" alt="Kanba company logo" />
+              <div class="d-flex flex-column flex-sm-row align-items-start align-items-sm-center">
+                <h6 class="mb-0 mr-2">Shelley McNabb</h6>
+                <span>Software Engineer</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="divider divider-bottom bg-white"></div>
+      </section>
     </Layout>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Layout from '../components/layout'
 import Link from 'next/link'
 import { DefaultSeo } from 'next-seo';
+import take_flight from "../components/take_flight";
 
 export default function Home() {
     return (
@@ -69,67 +70,7 @@ export default function Home() {
                         </div>
                     </div>
                 </section>
-                <section class="bg-primary-3 text-white">
-                    <div class="container">
-                        <div class="row section-title justify-content-center text-center">
-                            <div class="col-md-9 col-lg-8 col-xl-7">
-                                <h3 class="display-4">Why are we diffirent?</h3>
-                            </div>
-                        </div>
-                        <div class="row justify-content-center">
-                            <div class="col d-flex flex-wrap justify-content-center">
-                                <div class="m-2 text-dark">
-                                    <div class="media rounded align-items-center pl-3 pr-4 pl-md-4 pr-md-5 py-2 bg-white text-dark">
-                                        <img src={"img/icons/theme/navigation/route.svg"} alt="Fully responsive" class="icon mr-3 bg-primary" />
-                                        <h5 class="mb-0">Fully Responsive</h5>
-                                    </div>
-                                </div>
-                                <div class="m-2 text-dark">
-                                    <div class="media rounded align-items-center pl-3 pr-4 pl-md-4 pr-md-5 py-2 bg-white text-dark">
-                                        <img src={"img/icons/theme/layout/layout-left-panel-2.svg"} alt="Layouts icon" class="icon mr-3 bg-primary" />
-                                        <h5 class="mb-0">Multiple Layouts</h5>
-                                    </div>
-                                </div>
-                                <div class="m-2 text-dark">
-                                    <div class="media rounded align-items-center pl-3 pr-4 pl-md-4 pr-md-5 py-2 bg-white text-dark">
-                                        <img src={"img/icons/theme/shopping/box-2.svg"} alt="Box icon" class="icon mr-3 bg-primary" />
-                                        <h5 class="mb-0">Modular Components</h5>
-                                    </div>
-                                </div>
-                                <div class="m-2 text-dark">
-                                    <div class="media rounded align-items-center pl-3 pr-4 pl-md-4 pr-md-5 py-2 bg-white text-dark">
-                                        <img src={"img/icons/theme/general/thunder-move.svg"} alt="Lightning icon" class="icon mr-3 bg-primary" />
-                                        <h5 class="mb-0">Suits Your Style</h5>
-                                    </div>
-                                </div>
-                                <div class="m-2 text-dark">
-                                    <div class="media rounded align-items-center pl-3 pr-4 pl-md-4 pr-md-5 py-2 bg-white text-dark">
-                                        <img src={"img/icons/theme/general/bookmark.svg"} alt="Bookmark icon" class="icon mr-3 bg-primary" />
-                                        <h5 class="mb-0">Well Documented</h5>
-                                    </div>
-                                </div>
-                                <div class="m-2 text-dark">
-                                    <div class="media rounded align-items-center pl-3 pr-4 pl-md-4 pr-md-5 py-2 bg-white text-dark">
-                                        <img src={"img/icons/theme/design/select.svg"} alt="Selection interface icon" class="icon mr-3 bg-primary" />
-                                        <h5 class="mb-0">Highly Customizable</h5>
-                                    </div>
-                                </div>
-                                <div class="m-2 text-dark">
-                                    <div class="media rounded align-items-center pl-3 pr-4 pl-md-4 pr-md-5 py-2 bg-white text-dark">
-                                        <img src={"img/icons/theme/general/fire.svg"} alt="Fire icon" class="icon mr-3 bg-primary" />
-                                        <h5 class="mb-0">Saves You Time</h5>
-                                    </div>
-                                </div>
-                                <div class="m-2 text-dark">
-                                    <div class="media rounded align-items-center pl-3 pr-4 pl-md-4 pr-md-5 py-2 bg-white text-dark">
-                                        <img src={"img/icons/theme/food/sushi.svg"} alt="Sushi icon" class="icon mr-3 bg-primary" />
-                                        <h5 class="mb-0">Delicious Stuff</h5>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+                <take_flight />
                 <section class="bg-light o-hidden">
                     <div class="container">
                         <div class="row section-title justify-content-center text-center">
