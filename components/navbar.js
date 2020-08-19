202d8d1ca3faae6efcfb40759f7263dfb0f735ac
mrgkanev <contact@mrgkanev.eu>
2020-08-19 16:31:39 +0300

    Updating logo

--- components/navbar.js
+++ components/navbar.js
@@ -6,7 +6,7 @@ export default function Navbar(){
             <nav className="navbar navbar-expand-lg navbar-dark" data-sticky="top">
                 <div className="container">
                     <a className="navbar-brand navbar-brand-dynamic-color fade-page" href="/">
-                        <img alt="Bimbala logo" src={"/img/logos/logo.svg"} height={"60vh"} className={"pr-4 d-inline"}/>
+                        <img alt="Bimbala logo" src={"/img/logos/logov3.svg"} height={"60vh"} className={"pr-4 d-inline"}/>
                         <h3 className={"d-inline"}>Bimbala</h3>
                     </a>
                     <div className="d-flex align-items-center order-lg-3">
