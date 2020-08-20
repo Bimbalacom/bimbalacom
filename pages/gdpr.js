d01a7fa09fedc13be9138a6471b06cf7be898d84
mrgkanev <contact@mrgkanev.eu>
2020-08-20 22:19:39 +0300

    Alfa version of our GDPR

--- pages/gdpr.js
+++ pages/gdpr.js
@@ -1,11 +1,159 @@
 import Layout from "../components/layout";
-import ComingSoon from "../components/comingSoon";
 import React from "react";
 
 export default function GDPR() {
     return (
         <Layout title={'GDPR - Bimbala.com'}>
-<ComingSoon/>
-    </Layout>
+            <section className="bg-light">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-xl-8 col-lg-9 col-md-11">
+                            <div className="card card-body shadow">
+                                <div className="d-flex flex-column justify-content-between align-items-start pb-4 mb-4 mb-md-5 border-bottom">
+                                    <div className="mb-3">
+                                        <h1 className="mb-2">GDPR Policy</h1>
+                                        <div className="lead">Updated 20 August, 2020</div>
+                                    </div>
+                                    <a href="javascript:if(window.print)window.print()" className="btn btn-primary">Print</a>
+                                </div>
+                                <article className="article">
+                                    <p>Definitions</p>
+                                    <ul>
+                                        <li>
+                                            <p>Bimbala<br />Company or simular means Bimbala Ltd.<br /><br /></p>
+                                        </li>
+                                        <li>
+                                            <p>GDPR<br />General Data Protection Regulation Act.<br /><br /></p>
+                                        </li>
+                                        <li>
+                                            <p>Data Controller<br />Data Controller means the natural or legal person who (either alone or jointly or in common with other persons) determines the purposes for which and the manner in which any personal information are, or are to be, processed.<br /><br /></p>
+                                        </li>
+                                        <li>
+                                            <p>Data Processor<br />Data Processor means any natural or legal person who processes the data on behalf of the Data Controller.<br /><br /></p>
+                                        </li>
+                                        <li>
+                                            <p>Data Subject<br />Data Subject is any living individual who is using our Service and is the subject of Personal Data.</p>
+                                        </li>
+                                    </ul>
+                                    <p></p>
+                                    <p>1. Principles for processing personal data</p>
+                                    <p>Our principles for processing personal data are:</p>
+                                    <p></p>
+                                    <ul>
+                                        <li>
+                                            <p>Fairness and lawfulness. When we process personal data, the individual rights of the Data Subjects must be protected. All personal data must be collected and processed in a legal and fair manner.</p>
+                                        </li>
+                                    </ul>
+                                    <p></p>
+                                    <ul>
+                                        <li>
+                                            <p>Restricted to a specific purpose. The personal data of Data Subject must be processed only for specific purposes.</p>
+                                        </li>
+                                    </ul>
+                                    <p></p>
+                                    <ul>
+                                        <li>
+                                            <p>Transparency. The Data Subject must be informed of how his/her data is being collected, processed and used.</p>
+                                        </li>
+                                    </ul>
+                                    <p></p>
+                                    <p>2. What personal data we collect and process</p>
+                                    <p></p>
+                                    <p>Bimbala collects several different types of personal data for various purposes. Personal Data may include, but is not limited to:</p>
+                                    <p></p>
+                                    <ul>
+                                        <li>
+                                            <p>Email address</p>
+                                        </li>
+                                        <li>
+                                            <p>First name and last name</p>
+                                        </li>
+                                        <li>
+                                            <p>Phone number</p>
+                                        </li>
+                                        <li>
+                                            <p>Address, State, Province, ZIP/Postal code, City</p>
+                                        </li>
+                                    </ul>
+                                    <p></p>
+                                    <p>3. How we use the personal data</p>
+                                    <p></p>
+                                    <p>Bimbala uses the collected personal data for various purposes:</p>
+                                    <p></p>
+                                    <ul>
+                                        <li>
+                                            <p>To provide you with services</p>
+                                        </li>
+                                        <li>
+                                            <p>To notify you about changes to our services and/or products</p>
+                                        </li>
+                                        <li>
+                                            <p>To provide customer support</p>
+                                        </li>
+                                        <li>
+                                            <p>To gather analysis or valuable information so that we can improve our services</p>
+                                        </li>
+                                        <li>
+                                            <p>To detect, prevent and address technical issues</p>
+                                        </li>
+                                    </ul>
+                                    <p></p>
+                                    <p>4. Legal basis for collecting and processing personal data</p>
+                                    <p></p>
+                                    <p>Bimbala legal basis for collecting and using the personal data described in this Data Protection Policy depends on the personal data we collect and the specific context in which we collect the information:</p>
+                                    <p></p>
+                                    <ul>
+                                        <li>
+                                            <p>Bimbala needs to perform a contract with you</p>
+                                        </li>
+                                        <li>
+                                            <p>You have given Bimbala permission to do so</p>
+                                        </li>
+                                        <li>
+                                            <p>Processing your personal data is in Bimbala legitimate interests</p>
+                                        </li>
+                                        <li>
+                                            <p>Bimbala needs to comply with the law</p>
+                                        </li>
+                                    </ul>
+                                    <p></p>
+                                    <p>5. Retention of personal data</p>
+                                    <p></p>
+                                    <p>Bimbala will retain your personal information only for as long as is necessary for the purposes set out in this Data Protection Policy.</p>
+                                    <p></p>
+                                    <p>Bimbala will retain and use your information to the extent necessary to comply with our legal obligations, resolve disputes, and enforce our policies.</p>
+                                    <p></p>
+                                    <p>6. Data protection rights</p>
+                                    <p></p>
+                                    <p>If you are a resident of the European Economic Area (EEA), you have certain data protection rights. If you wish to be informed what personal data we hold about you and if you want it to be removed from our systems, please contact us.</p>
+                                    <p></p>
+                                    <p>In certain circumstances, you have the following data protection rights:</p>
+                                    <p></p>
+                                    <ul>
+                                        <li>
+                                            <p>The right to access, update or to delete the information we have on you</p>
+                                        </li>
+                                        <li>
+                                            <p>The right of rectification</p>
+                                        </li>
+                                        <li>
+                                            <p>The right to object</p>
+                                        </li>
+                                        <li>
+                                            <p>The right of restriction</p>
+                                        </li>
+                                        <li>
+                                            <p>The right to data portability</p>
+                                        </li>
+                                        <li>
+                                            <p>The right to withdraw consent</p>
+                                        </li>
+                                    </ul>
+                                </article></div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </Layout>
     )
 }
