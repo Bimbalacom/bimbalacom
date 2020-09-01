585790ad293087b29e0a64dd215a5fc7753eddf7
mrgkanev <contact@mrgkanev.eu>
2020-09-01 21:30:25 +0300

    Testing v2

--- components/take_flight.js
+++ components/take_flight.js
@@ -13,37 +13,37 @@ export default function TakeFlight() {
                 <div className="col d-flex flex-wrap justify-content-center">
                     <div className="m-2 text-dark">
                         <div className="media rounded align-items-center pl-3 pr-4 pl-md-4 pr-md-5 py-2 bg-white text-dark">
-                            <img src={"img/icons/theme/navigation/route.svg"} alt="Fully responsive" className="icon mr-3" />
+                            <img src={"img/icons/theme/navigation/route.svg"} loading="lazy" alt="Fully responsive" className="icon mr-3" />
                             <h5 className="mb-0">Fully Responsive</h5>
                         </div>
                     </div>
                     <div className="m-2 text-dark">
                         <div className="media rounded align-items-center pl-3 pr-4 pl-md-4 pr-md-5 py-2 bg-white text-dark">
-                            <img src={"img/icons/theme/layout/layout-left-panel-2.svg"} alt="Layouts icon" className="icon mr-3" />
+                            <img src={"img/icons/theme/layout/layout-left-panel-2.svg"} loading="lazy" alt="Layouts icon" className="icon mr-3" />
                             <h5 className="mb-0">Multiple Layouts</h5>
                         </div>
                     </div>
                     <div className="m-2 text-dark">
                         <div className="media rounded align-items-center pl-3 pr-4 pl-md-4 pr-md-5 py-2 bg-white text-dark">
-                            <img src={"img/icons/theme/shopping/box-2.svg"} alt="Box icon" className="icon mr-3" />
+                            <img src={"img/icons/theme/shopping/box-2.svg"} loading="lazy" alt="Box icon" className="icon mr-3" />
                             <h5 className="mb-0">Modular Components</h5>
                         </div>
                     </div>
                     <div className="m-2 text-dark">
                         <div className="media rounded align-items-center pl-3 pr-4 pl-md-4 pr-md-5 py-2 bg-white text-dark">
-                            <img src={"img/icons/theme/general/thunder-move.svg"} alt="Lightning icon" className="icon mr-3" />
+                            <img src={"img/icons/theme/general/thunder-move.svg"} loading="lazy" alt="Lightning icon" className="icon mr-3" />
                             <h5 className="mb-0">Suits Your Style</h5>
                         </div>
                     </div>
                     <div className="m-2 text-dark">
                         <div className="media rounded align-items-center pl-3 pr-4 pl-md-4 pr-md-5 py-2 bg-white text-dark">
-                            <img src={"img/icons/theme/general/bookmark.svg"} alt="Bookmark icon" className="icon mr-3" />
+                            <img src={"img/icons/theme/general/bookmark.svg"} loading="lazy" alt="Bookmark icon" className="icon mr-3" />
                             <h5 className="mb-0">Well Documented</h5>
                         </div>
                     </div>
                     <div className="m-2 text-dark">
                         <div className="media rounded align-items-center pl-3 pr-4 pl-md-4 pr-md-5 py-2 bg-white text-dark">
-                            <img src={"img/icons/theme/design/select.svg"} alt="Selection interface icon" className="icon mr-3" />
+                            <img src={"img/icons/theme/design/select.svg"} loading="lazy" alt="Selection interface icon" className="icon mr-3" />
                             <h5 className="mb-0">Highly Customizable</h5>
                         </div>
                     </div>
