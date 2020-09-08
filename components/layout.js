bfd5b5b5f6592a206e45543e14e4afc5df666c13
Lyuboslav Alexandrov <alexandrovcorp@yahoo.com>
2020-09-09 01:08:19 +0300

    Remove all vendor JS

--- components/layout.js
+++ components/layout.js
@@ -39,37 +39,37 @@ export default class Layout extends React.Component {
         {/* Optional Vendor Scripts (Remove the plugin script here and comment initializer script out of index.js if site does not use that feature) */}
 
         {/* AOS (Animate On Scroll - animates elements into view while scrolling down) */}
-        <script type="text/javascript" src="/js/aos.js" />
-        {/* Clipboard (copies content from browser into OS clipboard) */}
-        <script type="text/javascript" src="/js/clipboard.min.js" />
-        {/* Fancybox (handles image and video lightbox and galleries) */}
-        <script type="text/javascript" src="/js/jquery.fancybox.min.js" />
-        {/* Flickity (handles touch enabled carousels and sliders) */}
-        <script type="text/javascript" src="/js/flickity.pkgd.min.js" />
-        {/* Ion rangeSlider (flexible and pretty range slider elements) */}
-        <script type="text/javascript" src="/js/ion.rangeSlider.min.js" />
-        {/* Isotope (masonry layouts and filtering) */}
-        <script type="text/javascript" src="/js/isotope.pkgd.min.js" />
-        {/* jarallax (parallax effect and video backgrounds) */}
-        <script type="text/javascript" src="/js/jarallax.min.js" />
-        <script type="text/javascript" src="/js/jarallax-video.min.js" />
-        <script type="text/javascript" src="/js/jarallax-element.min.js" />
-        {/* jQuery Countdown (displays countdown text to a specified date) */}
-        <script type="text/javascript" src="/js/jquery.countdown.min.js" />
-        {/* jQuery smartWizard facilitates steppable wizard content */}
-        <script type="text/javascript" src="/js/jquery.smartWizard.min.js" />
-        {/* Plyr (unified player for Video, Audio, Vimeo and Youtube) */}
-        <script type="text/javascript" src="/js/plyr.polyfilled.min.js" />
-        {/* Prism (displays formatted code boxes) */}
-        <script type="text/javascript" src="/js/prism.js"/>
-        {/* ScrollMonitor (manages events for elements scrolling in and out of view) */}
-        <script type="text/javascript" src="/js/scrollMonitor.js" />
-        {/* Smooth scroll (animation to links in-page)*/}
-        <script type="text/javascript" src="/js/smooth-scroll.polyfills.min.js" />
-        {/* Typed text (animated typing effect)*/}
-        <script type="text/javascript" src="/js/typed.min.js" />
-        {/* Required theme scripts (Do not remove) */}
-        <script type="text/javascript" src="/js/theme.js" async/>
+        {/*<script type="text/javascript" src="/js/aos.js" />*/}
+        {/*/!* Clipboard (copies content from browser into OS clipboard) *!/*/}
+        {/*<script type="text/javascript" src="/js/clipboard.min.js" />*/}
+        {/*/!* Fancybox (handles image and video lightbox and galleries) *!/*/}
+        {/*<script type="text/javascript" src="/js/jquery.fancybox.min.js" />*/}
+        {/*/!* Flickity (handles touch enabled carousels and sliders) *!/*/}
+        {/*<script type="text/javascript" src="/js/flickity.pkgd.min.js" />*/}
+        {/*/!* Ion rangeSlider (flexible and pretty range slider elements) *!/*/}
+        {/*<script type="text/javascript" src="/js/ion.rangeSlider.min.js" />*/}
+        {/*/!* Isotope (masonry layouts and filtering) *!/*/}
+        {/*<script type="text/javascript" src="/js/isotope.pkgd.min.js" />*/}
+        {/*/!* jarallax (parallax effect and video backgrounds) *!/*/}
+        {/*<script type="text/javascript" src="/js/jarallax.min.js" />*/}
+        {/*<script type="text/javascript" src="/js/jarallax-video.min.js" />*/}
+        {/*<script type="text/javascript" src="/js/jarallax-element.min.js" />*/}
+        {/*/!* jQuery Countdown (displays countdown text to a specified date) *!/*/}
+        {/*<script type="text/javascript" src="/js/jquery.countdown.min.js" />*/}
+        {/*/!* jQuery smartWizard facilitates steppable wizard content *!/*/}
+        {/*<script type="text/javascript" src="/js/jquery.smartWizard.min.js" />*/}
+        {/*/!* Plyr (unified player for Video, Audio, Vimeo and Youtube) *!/*/}
+        {/*<script type="text/javascript" src="/js/plyr.polyfilled.min.js" />*/}
+        {/*/!* Prism (displays formatted code boxes) *!/*/}
+        {/*<script type="text/javascript" src="/js/prism.js"/>*/}
+        {/*/!* ScrollMonitor (manages events for elements scrolling in and out of view) *!/*/}
+        {/*<script type="text/javascript" src="/js/scrollMonitor.js" />*/}
+        {/*/!* Smooth scroll (animation to links in-page)*!/*/}
+        {/*<script type="text/javascript" src="/js/smooth-scroll.polyfills.min.js" />*/}
+        {/*/!* Typed text (animated typing effect)*!/*/}
+        {/*<script type="text/javascript" src="/js/typed.min.js" />*/}
+        {/*/!* Required theme scripts (Do not remove) *!/*/}
+        {/*<script type="text/javascript" src="/js/theme.js" async/>*/}
     </React.Fragment>
     }
 }
