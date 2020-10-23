26f7621aff4bedba158fc913f7be9bc7a3cd2fd8
Gabriel Kanev <contact@mrgkanev.eu>
2020-10-23 21:38:17 +0300

    Uploading photos

--- pages/features/frequently-asked-questions.js
+++ pages/features/frequently-asked-questions.js
@@ -5,18 +5,18 @@ import TakeFlight from "../../components/take_flight";
 
 export default function FirstPost() {
   return (
-    <Layout title={'Knowledge Board'}>
+    <Layout title={'Frequently Asked Questions'}>
     <section className="o-hidden">
         <div className="container">
           <div className="row align-items-center justify-content-around text-center text-lg-left">
             <div className="col-md-9 col-lg-6 col-xl-5 mb-4 mb-md-5 mb-lg-0 pl-lg-5 pl-xl-0">
               <div>
-                <h3 className="h1">Tell the people how is done</h3>
-                <p className="lead">People love to learn. Why don't you provide them with information that they need they will love you even more.</p>
+                <h3 className="h1">Everybody has questions</h3>
+                <p className="lead">Isn't it better to help your clients out ?!? People can read information from everywhere but you can give them what they exactly need.</p>
               </div>
             </div>
             <div className="col-md-9 col-lg-6 col-xl-5">
-              <img src={"../img/illustrations/tell-how-is-done.svg"} alt="Love to learn" className="img-fluid" />
+              <img src={"../img/illustrations/faq.svg"} alt="FAQ" className="img-fluid" />
             </div>
           </div>
         </div>
@@ -26,12 +26,12 @@ export default function FirstPost() {
           <div className="row align-items-center justify-content-around text-center text-lg-left">
             <div className="col-md-9 col-lg-6 col-xl-5 mb-4 mb-md-5 mb-lg-0 order-lg-2 pl-lg-5 pl-xl-0">
               <div>
-                <h2 className="h1">Help your team work / party more</h2>
-                <p className="lead">Developers / Support / Managers love they're jobs. Don't make them do stuff 4-5 times just because you don't have a good system behind you. Help yourself by helping them.</p>
+                <h2 className="h1">Questions the experts</h2>
+                <p className="lead">You are the best source of information about your software / startup. Giving the right answers when it counts is something that people remember.</p>
               </div>
             </div>
             <div className="col-md-9 col-lg-6 col-xl-5 order-lg-1">
-              <img src={"../img/illustrations/party.svg"} alt="Help your team party / work" className="img-fluid" />
+              <img src={"../img/illustrations/questions.svg"} alt="Questions the experts" className="img-fluid" />
             </div>
           </div>
         </div>
@@ -41,12 +41,12 @@ export default function FirstPost() {
           <div className="row align-items-center justify-content-around text-center text-lg-left">
             <div className="col-md-9 col-lg-6 col-xl-5 mb-4 mb-md-5 mb-lg-0 pl-lg-5 pl-xl-0">
               <div>
-                <h3 className="h1">Files away</h3>
+                <h3 className="h1">Let's vote it out</h3>
                 <p className="lead">Not everything can be explained by only text. We support uploading images/files/etc.</p>
               </div>
             </div>
             <div className="col-md-9 col-lg-6 col-xl-5">
-              <img src={"../img/illustrations/documents.svg"} alt="Files away" className="img-fluid" />
+              <img src={"../img/illustrations/voting.svg"} alt="Voting" className="img-fluid" />
             </div>
           </div>
         </div>
@@ -61,7 +61,7 @@ export default function FirstPost() {
               </div>
             </div>
             <div className="col-md-9 col-lg-6 col-xl-5 order-lg-1">
-              <img src={"../img/illustrations/work_time.svg"} alt="Work Time" className="img-fluid" />
+              <img src={"../img/illustrations/detailed_information.svg"} alt="Work Time" className="img-fluid" />
             </div>
           </div>
         </div>
