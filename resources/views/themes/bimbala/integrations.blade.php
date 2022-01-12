b6ea29fae25426c6576ceb4365061055b433b495
Gabriel Kanev <contact@mrgkanev.eu>
2022-01-12 21:27:11 +0200

    Finalizing integrations

--- resources/views/themes/bimbala/integrations.blade.php
+++ resources/views/themes/bimbala/integrations.blade.php
@@ -1,123 +1,39 @@
 @extends('theme::layouts.app')
 
 @section('content')
-
-{{--Big in progress --}}
-<section class="bg-gray-100">
-    <div class="relative">
-        <div class="relative px-6 py-8 ml-auto mr-auto bg-top bg-cover sm:py-16 max-w-7xl md:px-24 lg:px-16 lg:py-20">
-            <div class="relative grid gap-6 bg-top bg-cover sm:grid-cols-2 lg:grid-cols-4">
-                <div class="flex flex-col items-start justify-between p-6 space-y-4 overflow-hidden transition-shadow duration-200 bg-white bg-top bg-cover border border-gray-100 shadow-xl rounded-2xl group hover:shadow-2xl">
-                    <div class="flex items-center justify-center w-10 h-10 text-center bg-top bg-cover rounded-full bg-indigo-50">
-                        <p class="relative">
-                            <svg class="w-5 h-5 text-indigo-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z"></path></svg>
-                        </p>
+    {{-- Content --}}
+    <section class="w-full py-12 bg-white lg:py-24">
+    {{-- Integrations --}}
+        <section class="py-12 sm:py-16 bg-white">
+            <div class="max-w-7xl px-10 mx-auto sm:text-center">
+                <h3 class="font-bold text-3xl sm:text-3xl lg:text-4xl mt-3">Connect with Your Favorite Apps.</h3>
+                <p class="mt-4 text-gray-500 text-base sm:text-xl lg:text-2xl">We're flexible.<br class="lg:hidden hidden sm:block"> Check out with who we are integrated  👇</p>
+                <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 my-12 sm:my-16">
+                    <div class="rounded-lg py-10 flex flex-col items-center justify-center shadow-lg border border-gray-100">
+                    <img class="w-16 h-auto" src="{{ asset('themes\bimbala\images\integrations\google-tag-manager.svg') }}" loading=”lazy” alt="Google Tag Manager">
+                        <p class="font-bold mt-4">Google Tag Manager</p>
+                        <p class="mt-2 text-sm text-gray-500">Web Tag Manager</p>
                     </div>
-                    <p class="font-bold text-gray-700">User Manager</p>
-                    <p class="text-sm leading-5 text-gray-500">Easily manage the users of your application. Allow access to specific areas and sections.</p>
-                </div>
-                <div class="flex flex-col items-start justify-between p-6 space-y-4 overflow-hidden transition-shadow duration-200 bg-white bg-top bg-cover border border-gray-100 shadow-xl rounded-2xl group hover:shadow-2xl">
-                    <div class="flex items-center justify-center w-10 h-10 text-center bg-top bg-cover rounded-full bg-indigo-50">
-                        <p class="relative">
-                            <svg class="w-5 h-5 text-indigo-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z" clip-rule="evenodd"></path><path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z"></path></svg>
-                        </p>
+                    <div class="rounded-lg py-10 flex flex-col items-center justify-center shadow-lg border border-gray-100">
+                    <img class="w-16 h-auto" src="{{ asset('themes\bimbala\images\integrations\mailchimp.svg') }}" loading=”lazy” alt="Mailchimp">
+                        <p class="font-bold mt-4">Mailchimp *</p>
+                        <p class="mt2 text-sm text-gray-500">Email Marketing</p>
                     </div>
-                    <p class="font-bold text-gray-700">Projects</p>
-                    <p class="text-sm leading-5 text-gray-500">Unlimted projects for you and your team. Easily create, modify, and duplicate your projects.</p>
-                </div>
-                <div class="flex flex-col items-start justify-between p-6 space-y-4 overflow-hidden transition-shadow duration-200 bg-white bg-top bg-cover border border-gray-100 shadow-xl rounded-2xl group hover:shadow-2xl">
-                    <div class="flex items-center justify-center w-10 h-10 text-center bg-top bg-cover rounded-full bg-indigo-50">
-                        <p class="relative">
-                            <svg class="w-5 h-5 text-indigo-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M2 5a2 2 0 012-2h12a2 2 0 012 2v10a2 2 0 01-2 2H4a2 2 0 01-2-2V5zm3.293 1.293a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 01-1.414-1.414L7.586 10 5.293 7.707a1 1 0 010-1.414zM11 12a1 1 0 100 2h3a1 1 0 100-2h-3z" clip-rule="evenodd"></path></svg>
-                        </p>
+                    <div class="rounded-lg py-10 flex flex-col items-center justify-center shadow-lg border border-gray-100">
+                    <img class="w-16 h-auto" src="{{ asset('themes\bimbala\images\integrations\Hubspot.svg') }}" loading=”lazy” alt="Hubspot">
+                        <p class="font-bold mt-4">Hubspot *</p>
+                        <p class="mt-2 text-sm text-gray-500">Customer Relations</p>
                     </div>
-                    <p class="font-bold text-gray-700">Developer API</p>
-                    <p class="text-sm leading-5 text-gray-500">Well documented developer API that allows you to build on top of our platform with ease.</p>
-                </div>
-                <div class="flex flex-col items-start justify-between p-6 space-y-4 overflow-hidden transition-shadow duration-200 bg-white bg-top bg-cover border border-gray-100 shadow-xl rounded-2xl group hover:shadow-2xl">
-                    <div class="flex items-center justify-center w-10 h-10 text-center bg-top bg-cover rounded-full bg-indigo-50">
-                        <p class="relative">
-                            <svg class="w-5 h-5 text-indigo-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M3 4a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zM3 10a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H4a1 1 0 01-1-1v-6zM14 9a1 1 0 00-1 1v6a1 1 0 001 1h2a1 1 0 001-1v-6a1 1 0 00-1-1h-2z"></path></svg>
-                        </p>
+                    <div class="rounded-lg py-10 flex flex-col items-center justify-center shadow-lg border border-gray-100">
+                    <img class="w-16 h-auto" src="{{ asset('themes\bimbala\images\integrations\zendesk.svg') }}" loading=”lazy” alt="Zendesk">
+                        <p class="font-bold mt-4">Zendesk *</p>
+                        <p class="mt-2 text-sm text-gray-500">Customer Messaging</p>
                     </div>
-                    <p class="font-bold text-gray-700">Template Designs</p>
-                    <p class="text-sm leading-5 text-gray-500">Templates and designs for your next project. You can easily drop these templates into any project.</p>
                 </div>
-                <div class="flex flex-col items-start justify-between p-6 space-y-4 overflow-hidden transition-shadow duration-200 bg-white bg-top bg-cover border border-gray-100 shadow-xl rounded-2xl group hover:shadow-2xl">
-                    <div class="flex items-center justify-center w-10 h-10 text-center bg-top bg-cover rounded-full bg-indigo-50">
-                        <p class="relative">
-                            <svg class="w-5 h-5 text-indigo-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 3a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2V5a2 2 0 00-2-2H5zm9 4a1 1 0 10-2 0v6a1 1 0 102 0V7zm-3 2a1 1 0 10-2 0v4a1 1 0 102 0V9zm-3 3a1 1 0 10-2 0v1a1 1 0 102 0v-1z" clip-rule="evenodd"></path></svg>
-                        </p>
-                    </div>
-                    <p class="font-bold text-gray-700">Analytics</p>
-                    <p class="text-sm leading-5 text-gray-500">User and customer analytics to help you understand which areas of your application are being used.</p>
-                </div>
-                <div class="flex flex-col items-start justify-between p-6 space-y-4 overflow-hidden transition-shadow duration-200 bg-white bg-top bg-cover border border-gray-100 shadow-xl rounded-2xl group hover:shadow-2xl">
-                    <div class="flex items-center justify-center w-10 h-10 text-center bg-top bg-cover rounded-full bg-indigo-50">
-                        <p class="relative">
-                            <svg class="w-5 h-5 text-indigo-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clip-rule="evenodd"></path></svg>
-                        </p>
-                    </div>
-                    <p class="font-bold text-gray-700">Filters</p>
-                    <p class="text-sm leading-5 text-gray-500">Filter your results by the criteria that is the most important and most vital to your business.</p>
-                </div>
-                <div class="flex flex-col items-start justify-between p-6 space-y-4 overflow-hidden transition-shadow duration-200 bg-white bg-top bg-cover border border-gray-100 shadow-xl rounded-2xl group hover:shadow-2xl">
-                    <div class="flex items-center justify-center w-10 h-10 text-center bg-top bg-cover rounded-full bg-indigo-50">
-                        <p class="relative">
-                            <svg class="w-5 h-5 text-indigo-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z"></path></svg>
-                        </p>
-                    </div>
-                    <p class="font-bold text-gray-700">Customizations</p>
-                    <p class="text-sm leading-5 text-gray-500">Customize every aspect of the user interface. You can also customize the internal functionality.</p>
-                </div>
-                <div class="flex flex-col items-start justify-between p-6 space-y-4 overflow-hidden transition-shadow duration-200 bg-white bg-top bg-cover border border-gray-100 shadow-xl rounded-2xl group hover:shadow-2xl">
-                    <div class="flex items-center justify-center w-10 h-10 text-center bg-top bg-cover rounded-full bg-indigo-50">
-                        <p class="relative">
-                            <svg class="w-5 h-5 text-indigo-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 3.5a1.5 1.5 0 013 0V4a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-.5a1.5 1.5 0 000 3h.5a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-.5a1.5 1.5 0 00-3 0v.5a1 1 0 01-1 1H6a1 1 0 01-1-1v-3a1 1 0 00-1-1h-.5a1.5 1.5 0 010-3H4a1 1 0 001-1V6a1 1 0 011-1h3a1 1 0 001-1v-.5z"></path></svg>
-
-                        </p>
-                    </div>
-                    <p class="font-bold text-gray-700">Integrations</p>
-                    <p class="text-sm leading-5 text-gray-500">We have also built some amazing and powerful integrations to help you build quicker.</p>
-                </div>
+                <p class="pb-8 text-purple-900 font-medium uppercase">Want more integrations? Please tell us !</p> 
+                <a href="https://board.bimbala.com/" class="w-full px-8 py-3 text-lg text-center bg-purple-900 text-white transition duration-150 ease-in-out bg-white sm:w-auto focus:outline-none rounded">Our roadmap</a>
             </div>
-        </div>
-    </div>
-</section>
-
-{{--Another one --}}
-
-<section class="bg-white">
-    <div class="max-w-6xl py-12 mx-auto">
-        <div class="grid gap-8 md:grid-cols-2 lg:gap-12 ">
-            <a href="#_" class="flex flex-col p-6 space-y-6 transition-all duration-500 bg-white border border-indigo-100 rounded-lg shadow hover:shadow-xl lg:p-8 lg:flex-row lg:space-y-0 lg:space-x-6">
-                <div class="flex items-center justify-center w-16 h-16 border border-indigo-200 shadow-inner bg-gradient-to-br from-indigo-50 to-indigo-200 rounded-xl lg:h-20 lg:w-20">
-                    <svg class="w-10 h-10 text-indigo-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
-                </div>
-                <div class="flex-1">
-                    <h5 class="mt-1 mb-2 text-xl font-bold lg:text-2xl">Fully Customizable</h5>
-                    <p class="mb-6 text-lg text-gray-600">Each component is fully customizable</p>
-                    <span class="flex items-center text-lg font-bold text-indigo-600">
-                        Customzation Details
-                        <svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
-                    </span>
-                </div>
-            </a>
-            <a href="#_" class="flex flex-col p-6 space-y-6 transition-all duration-500 bg-white border border-indigo-100 rounded-lg shadow hover:shadow-xl lg:p-8 lg:flex-row lg:space-y-0 lg:space-x-6">
-                <div class="flex items-center justify-center w-16 h-16 border border-indigo-200 shadow-inner bg-gradient-to-br from-indigo-50 to-indigo-200 rounded-xl lg:h-20 lg:w-20">
-                    <svg class="w-10 h-10 text-indigo-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 4a2 2 0 114 0v1a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-1a2 2 0 100 4h1a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-1a2 2 0 10-4 0v1a1 1 0 01-1 1H7a1 1 0 01-1-1v-3a1 1 0 00-1-1H4a2 2 0 110-4h1a1 1 0 001-1V7a1 1 0 011-1h3a1 1 0 001-1V4z"></path></svg>
-                </div>
-                <div class="flex-1">
-                    <h5 class="mt-1 mb-2 text-xl font-bold lg:text-2xl">Integrates Easily</h5>
-                    <p class="mb-6 text-lg text-gray-600">Integrate each component with your stack</p>
-                    <span class="flex items-center text-lg font-bold text-indigo-600">
-                        Integration Details
-                        <svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
-                    </span>
-                </div>
-            </a>
-        </div>
-    </div>
-</section>
+        </section>
+{{--End of Integrations --}}
+    </section>
 @endsection
-
