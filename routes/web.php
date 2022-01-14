6023a651d5898e2dcd70eca283e4bc999696af75
Lyuboslav Alexandrov <lyuboslav@bimbala.com>
2022-01-15 00:55:46 +0200

    Fixed routes - Passing SEO information to views

--- routes/web.php
+++ routes/web.php
@@ -21,13 +21,22 @@ Route::group(['prefix' => 'admin'], function () {
 });
 
 // Bimbala included
-Route::view('/team','theme::team')->name('team');
-Route::view('/contact','theme::contact')->name('contact');
-Route::view('/about-us','theme::about_us')->name('about-us');
-Route::view('/try-it','theme::try_it')->name('try-it');
+Route::get('/team', static function () {
+    return view('theme::team', ['seo' => ['seo_title' => 'Our Team', 'seo_description' => 'Check out our awesome team which consists of 2 crazy jacks-of-all-trades men. You can easily connect with them.',
+            ]]);
+})->name('team');
+Route::get('/about-us', static function () {
+    return view('theme::about_us', ['seo' => ['seo_title' => 'About Us', 'seo_description' => 'Read our amazing company history. It all became as a joke and now we grow strong.',
+    ]]);
+})->name('about-us');
+Route::get('/try-it', static function () {
+    return view('theme::try_it', ['seo' => ['seo_title' => 'Try it', 'seo_description' => 'Try our product and be part of our beta testers group. We promise - you will love it!',
+    ]]);
+})->name('try-it');
 Route::redirect('/discord','https://discord.gg/tADx7aJusB');
 
-Route::post('/contact', ContactController::class)->name('contact-send');
+Route::get('/contact', [ContactController::class, 'serveThePage'])->name('contact');
+Route::post('/contact', [ContactController::class, 'sendEmail'])->name('contact-send');
 
 // Include Wave Routes
 Wave::routes();
