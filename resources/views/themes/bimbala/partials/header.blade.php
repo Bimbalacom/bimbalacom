5fdc94fddb992381c7b8fbf4ebe1024c5ee0fe79
Gabriel Kanev <contact@mrgkanev.eu>
2022-03-09 23:26:19 +0200

    Small update

--- resources/views/themes/bimbala/partials/header.blade.php
+++ resources/views/themes/bimbala/partials/header.blade.php
@@ -22,12 +22,10 @@
 
     <nav x-data="{ mobile: false }" class="relative z-40 px-10 pt-6 mx-auto md:pb-6 max-w-7xl md:flex md:justify-between md:items-center">
         <div class="relative z-20 flex items-center justify-between">
-            <div class="flex flex-row">
-                <a href="{{ route('wave.home') }}">
-                    <img class=" h-10 w-10" src="../../bimbala/img/logo.png" alt="Bimbala"> 
-                    {{-- <h2 class=" text-lg font-semibold text-white md:text-xl">Bimbala</h2> --}}
+            <a href="{{ route('wave.home') }}" class="relative flex items-center">
+                   <img class="h-10 w-10" src="../../bimbala/img/logo.png" alt="Bimbala"> 
+                    <h2 class="pl-10 text-lg font-semibold text-white md:text-xl">Bimbala</h2>
                 </a>
-            </div>
                 {{-- This is the homepage nav when a user is not logged in --}}
                 @if(auth()->guest())
                     @include('theme::menus.guest')
