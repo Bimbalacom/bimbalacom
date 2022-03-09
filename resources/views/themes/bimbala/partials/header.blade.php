41e45298e772be462f09bbff01399b1041d4b738
Gabriel Kanev <contact@mrgkanev.eu>
2022-03-09 23:15:28 +0200

    Adding logo

--- resources/views/themes/bimbala/partials/header.blade.php
+++ resources/views/themes/bimbala/partials/header.blade.php
@@ -22,9 +22,10 @@
 
     <nav x-data="{ mobile: false }" class="relative z-40 px-10 pt-6 mx-auto md:pb-6 max-w-7xl md:flex md:justify-between md:items-center">
         <div class="relative z-20 flex items-center justify-between">
-            <div>
-                <a href="{{ route('wave.home') }}" class="text-lg font-semibold text-white md:text-xl">
-                    <h2 class="d-inline mb-0 brand">Bimbala</h2>
+            <div class="flex flex-row">
+                <a href="{{ route('wave.home') }}">
+                    <img class=" h-10 w-10" src="../../bimbala/img/logo.png" alt="Bimbala"> 
+                    {{-- <h2 class=" text-lg font-semibold text-white md:text-xl">Bimbala</h2> --}}
                 </a>
             </div>
                 {{-- This is the homepage nav when a user is not logged in --}}
