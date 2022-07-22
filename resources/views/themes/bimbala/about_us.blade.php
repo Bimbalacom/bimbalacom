0f95a699812faa519fa5cd0463817e3bfb47882c
Lyuboslav Alexandrov <lyuboslav@bimbala.com>
2022-07-22 14:50:52 +0300

    Improved contrast in the "About us" page

--- resources/views/themes/bimbala/about_us.blade.php
+++ resources/views/themes/bimbala/about_us.blade.php
@@ -3,9 +3,7 @@
 @section('content')
     {{--Contact--}}
     <section class="w-full relative">
-        
         <div class="h-full inset-x-0 bottom-0 absolute"></div>
-        
         <div class="max-w-7xl px-12 py-12 lg:py-24  mx-auto text-center relative">
             <div class="space-y-12 text-justify">
                 <div class="max-w-3xl mb-20 space-y-5 sm:mx-auto sm:space-y-4">
@@ -18,7 +16,7 @@
                     <h3 class="relative text-3xl tracking-tight sm:text-4xl text-black mt-4">Target</h3>
                     <p class="text-xl text-black">
                         The product is carried mostly on the shoulders of two university students.
-                         Our dream is to succeed in addressing the needs of each business that carries about its customers' feedback. 
+                         Our dream is to succeed in addressing the needs of each business that carries about its customers' feedback.
                          This includes easier work process for managers as well as all desperate employees from a support team.
                     </p>
                 </div>
@@ -41,30 +39,30 @@
                 <!-- Stack 2 -->
                 <div class="col-span-4 w-full h-full "></div>
                 <div class="relative col-span-1 w-full h-full flex justify-center items-center ">
-                    <div class="h-full w-1 bg-white border-2 border-white-500"></div>
-                    <div class="absolute w-8 h-8 rounded-full bg-white z-10 text-black text-center border-2 border-white-500"></div>
+                    <div class="h-full w-1 bg-white border-2 border-gray-400"></div>
+                    <div class="absolute w-8 h-8 rounded-full bg-white z-10 text-black text-center border-2 border-gray-400"></div>
                 </div>
-                <div class="col-span-4 w-full h-full border-2 border-white-500">
-                    <div class="w-full h-full bg-white rounded-md p-2 md:pl-4">
-                        <h1 class="text-black text-2xl font-bold py-2">16.10.2021</h1>
-                        <p class="text-gray-500 sm:text-md text-sm">
+                <div class="col-span-4 w-full h-full">
+                    <div class="w-full h-full bg-gray-800 rounded-md p-2 md:pl-4">
+                        <h1 class="text-white text-2xl font-bold py-2">16.10.2021</h1>
+                        <p class="text-gray-200 sm:text-md text-sm">
                             After a long product and website development we started our first alpha testers seeking 🔎
                         </p>
                     </div>
                 </div>
 
                 <!-- Stack 1 -->
-                <div class="col-span-4 w-full h-full border-2 border-white-500">
-                    <div class="w-full h-full bg-white rounded-md p-2 md:pl-4">
-                        <h1 class="text-black text-2xl font-bold py-2">31.07.2020</h1>
-                        <p class="text-gray-500 sm:text-md text-sm">It all began with a coffee afternoon meeting between two students...
+                <div class="col-span-4 w-full h-full">
+                    <div class="w-full h-full bg-gray-800 rounded-md p-2 md:pl-4">
+                        <h1 class="text-white text-2xl font-bold py-2">31.07.2020</h1>
+                        <p class="text-gray-200 sm:text-md text-sm">It all began with a coffee afternoon meeting between two students...
                             Later in local pub the waitress drew the lots and set the name of this company. That is how Bimbala was born!🎉
                         </p>
                     </div>
                 </div>
                 <div class="relative col-span-1 w-full h-full flex justify-center items-center ">
-                    <div class="h-full w-1 bg-white border-2 border-white-500"></div>
-                    <div class="absolute w-8 h-8 rounded-full bg-white z-10 text-black text-center border-2 border-white-500 "></div>
+                    <div class="h-full w-1 bg-white border-2 border-gray-400"></div>
+                    <div class="absolute w-8 h-8 rounded-full bg-white z-10 text-black text-center border-2 border-gray-400"></div>
                 </div>
                 <div class="col-span-4 w-full h-full"></div>
             </div>
