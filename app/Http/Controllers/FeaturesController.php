27c1898054841f740f3d85cf25c8c58f1b998762
Lyuboslav Alexandrov <lyuboslav@bimbala.com>
2024-01-11 15:34:29 +0200

    Created routes and handlers

---
+++ app/Http/Controllers/FeaturesController.php
@@ -0,0 +1,70 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+
+class FeaturesController extends Controller
+{
+    /**
+     * @param Request $request
+     * @return Factory|View|Application
+     */
+    public function index(Request $request): Factory|View|Application
+    {
+        return view('theme::features', [
+            'seo' => [
+                'seo_title' => 'Features',
+                'seo_description' => 'Check out our features'
+            ]
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return Factory|View|Application
+     */
+    public function upvote(Request $request): Factory|View|Application
+    {
+        return view('theme::features.upvote', [
+            'seo' => [
+                'seo_title' => 'Upvote',
+                'seo_description' => 'Let your users give you product improvement suggestions, bringing their feedback to a next level',
+                'keywords' => 'upvote, product improvement, customer feedback, feedback organization, feedback voting, customer relationships, user requests, user ideas, user suggestions'
+            ]
+        ]);
+    }
+
+    /**
+     *
+     * @param Request $request
+     * @return Factory|View|Application
+     */
+    public function faq(Request $request): Factory|View|Application
+    {
+        return view('theme::features.faq', [
+            'seo' => [
+                'seo_title' => 'FAQ',
+                'seo_description' => 'Provide answers to all common questions'
+            ]
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return Factory|View|Application
+     */
+    public function roadmap(Request $request): Factory|View|Application
+    {
+        return view('theme::features.roadmap', [
+            'seo' => [
+                'seo_title' => 'Roadmap',
+                'seo_description' => 'Show your users their opinion matters'
+            ]
+        ]);
+    }
+}
