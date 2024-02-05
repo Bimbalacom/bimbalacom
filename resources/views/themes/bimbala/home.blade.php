2fd14d6483f2ad68f1cf1a458df23aa7e097bcd0
Gabriel Kanev <contact@mrgkanev.eu>
2024-02-05 13:03:21 +0200

    wip

--- resources/views/themes/bimbala/home.blade.php
+++ resources/views/themes/bimbala/home.blade.php
@@ -187,10 +187,61 @@
 </section>
 {{--End of Weekly mails --}}
 
+@component('theme::components.two-panel', ['title' => 'Welcome to Bimbala', 'subtitle' => 'Discover the power of feedback', 'image' => 'images/feedback.png', 'description' => 'Bimbala is an easy-to-use platform that allows you to collect and organize feedback from your clients. With Bimbala, you can gather valuable insights and make data-driven decisions to improve your products and services.'])
+@endcomponent
 {{-- Features with images --}}
-    <x-two-panel>
+    <section class="py-16">
+    <div class="flex flex-col px-8 mx-auto space-y-12 max-w-7xl xl:px-12">
+        <div class="relative">
+            <h2 class="w-full text-3xl font-bold text-center sm:text-4xl md:text-5xl"> Level Up Your Support</h2>
+            <p class="w-full py-8 mx-auto -mt-2 text-lg text-center intro sm:max-w-3xl">Helping your clients while, giving the needed tools for your support / marketing teams.</p>
+        </div>
+        <div class="flex flex-col mb-12 animated fadeIn sm:flex-row">
+            <div class="flex items-center mb-8 sm:w-1/2 md:w-6/12 sm:order-last">
+                <img class="rounded-lg" src="{{ asset('themes/bimbala/images/features/organize-feedback.jpg') }}" loading="lazy" alt="Gathering your client feedback">
+            </div>
+            <div class="flex flex-col justify-center mt-5 mb-8 md:mt-0 sm:w-1/2 md:w-6/12 sm:pr-16">
+                <p class="mb-2 text-sm font-semibold leading-none text-purple-700 uppercase">Gather your feedback</p>
+                <h3 class="mt-2 text-2xl md:text-4xl">An easy way to collect and organize feedback</h3>
+                <p class="mt-5 text-lg text md:">Gathering feedback is a big hassle. You receive it from everywhere and it gets lost in woords. That is why Bimbala is here to help and adapt to your way of thinking.</p>
+            </div>
+        </div>
+        <div class="flex flex-col mb-12 animated fadeIn sm:flex-row">
+                <div class="flex items-center mb-8 sm:w-1/2 md:w-6/12">
+                    <div class="relative w-full">
+                        <img src="{{ asset('themes/bimbala/images/features/collecting-feedback.png') }}" class="relative z-10 p-0 transform rounded-xl lg:scale-110" data-rounded="rounded-xl" data-rounded-max="rounded-full"  loading="lazy" alt="Make an optimized dedicated support portal">
 
-    </x-two-panel>
+                        <div class="absolute top-0 right-0 w-full h-full mt-4 -mr-5 transform scale-125 rotate-45">
+                            <svg class="absolute top-0 w-full h-full transform fill-current lg:scale-x-150 text-purple-50" data-primary="purple-600" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M43.5,-76.5C55.9,-68.2,65.2,-55.6,71.9,-42.1C78.5,-28.6,82.7,-14.3,81.3,-0.8C79.8,12.7,72.8,25.3,64.7,36.3C56.5,47.2,47.2,56.3,36.2,63.3C25.2,70.2,12.6,75,-1.1,76.9C-14.8,78.9,-29.7,78,-43.7,72.8C-57.7,67.6,-70.9,58.1,-79.3,45.2C-87.7,32.4,-91.2,16.2,-90.7,0.3C-90.3,-15.7,-85.9,-31.4,-76.4,-42.3C-66.9,-53.1,-52.3,-59.2,-38.7,-66.9C-25.1,-74.5,-12.6,-83.7,1.5,-86.3C15.6,-88.8,31.1,-84.8,43.5,-76.5Z" transform="translate(100 100)" />
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+            <div class="flex flex-col justify-center mt-5 mb-8 md:mt-0 sm:w-1/2 md:w-6/12 sm:pl-16">
+                <p class="mb-2 text-sm font-semibold leading-none text-left text-purple-700 uppercase">know your data</p>
+                <h3 class="mt-2 text-2xl sm:text-left md:text-4xl">Analyze and use the users' feedback</h3>
+                <p class="my-3 text-lg text-gray-700 text md:text-left">Prioritize your product roadmap with data directly from your feedback board. Share your roadmap with others.</p>
+                <p>
+                    <ul class="list-disc ml-5">
+                    <li>Real time input from users</li>
+                    <li>Data-Driven work approach</li>
+                </ul>
+            </p>
+            </div>
+        </div>
+        <div class="flex flex-col mb-12 animated fadeIn sm:flex-row">
+            <div class="flex items-center mb-8 sm:w-1/2 md:w-6/12 sm:order-last">
+                <img class="rounded-lg" src="{{ asset('themes/bimbala/images/features/better-desicion-making.jpg') }}" loading="lazy" alt="Better decision making">
+            </div>
+            <div class="flex flex-col justify-center mt-5 mb-8 md:mt-0 sm:w-1/2 md:w-6/12 sm:pr-16">
+                <p class="mb-2 text-sm font-semibold leading-none  text-purple-700 uppercase">Better decisions</p>
+                <h3 class="mt-2 text-2xl md:text-4xl">Better decision making</h3>
+                <p class="mt-5 text-lg text-gray-700">Asking your community is on of the best ways to know if you are going in the right path.</p>
+            </div>
+        </div>
+    </div>
+</section>
 
 {{-- End Features with images --}}
 {{-- Integrations --}}
