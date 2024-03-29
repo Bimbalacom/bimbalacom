415c43d4596694b74461f1e003db2e23e471043e
Gabriel Kanev <contact@mrgkanev.eu>
2024-03-29 16:26:22 +0200

    Update integration-card.blade.php

--- resources/views/themes/bimbala/components/integration-card.blade.php
+++ resources/views/themes/bimbala/components/integration-card.blade.php
@@ -1,5 +1,5 @@
 <div class="rounded-lg py-10 px-4 flex flex-col items-center justify-center shadow-lg border border-gray-100">
-    <img class="w-16 h-auto" src="{{ asset($image) }}" loading="lazy" alt="{{ $title }}" width="64px" height="64px">
+    <img class="w-16 h-auto" src="{{ asset($image) }}" alt="{{ $title }}" width="64px" height="64px" loading="lazy">
     <p class="font-bold mt-4">{{ $title }}</p>
     <p class="mt-2 text-sm">{{ $category }}</p>
     <p class="mt-4 text-sm">{{ $description }}</p>
