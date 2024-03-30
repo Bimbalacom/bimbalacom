6880fb0f28420e5867184cf1fe10276eabc8a12b
Lyuboslav Alexandrov <lyuboslav@bimbala.com>
2024-03-30 03:30:26 +0200

    Defer Clarity 2

--- resources/views/themes/bimbala/partials/footer.blade.php
+++ resources/views/themes/bimbala/partials/footer.blade.php
@@ -195,7 +195,7 @@
     <script type="text/javascript" defer async>
     (function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;t.defer=1;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
     })(window, document, "clarity", "script", "5kue80nmru");
     </script>
