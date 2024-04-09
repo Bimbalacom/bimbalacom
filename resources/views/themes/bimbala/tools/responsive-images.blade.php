e839a6d1224415edd36c8c8a5dfd87c18c9feed9
Lyuboslav Alexandrov <lyuboslav@bimbala.com>
2024-04-09 19:55:35 +0300

    Added text

--- resources/views/themes/bimbala/tools/responsive-images.blade.php
+++ resources/views/themes/bimbala/tools/responsive-images.blade.php
@@ -1,7 +1,10 @@
 @extends('theme::layouts.app')
 
 @section('content')
-    <div class="h-screen">
-        <iframe src="https://bimbala-responsive-images.netlify.app" width="100%" height="100%" frameborder="0">Your browser doesn't support iframes.</iframe>
+    <div class="h-screen overflow-hidden">
+        <div class="bg-gray-100 pt-12">
+            <p class="text-center text-xl prose-sm">Upload your picture (preferranbly large) and will generate you images for different breakpoints with optimal dimensions.</p>
+        </div>
+        <iframe src="https://bimbala-responsive-images.netlify.app" width="100%" height="100%" frameborder="0" loading="eager" class="overflow-hidden focus:outline-none" scrolling="no">Your browser doesn't support iframes.</iframe>
     </div>
 @endsection
