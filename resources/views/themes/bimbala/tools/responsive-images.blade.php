2b253eab01e1bd739e7fff2c1f484691e0cc51c8
Lyuboslav Alexandrov <lyuboslav@bimbala.com>
2024-04-10 13:33:20 +0300

    Update resources/views/themes/bimbala/tools/responsive-images.blade.php
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- resources/views/themes/bimbala/tools/responsive-images.blade.php
+++ resources/views/themes/bimbala/tools/responsive-images.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="bg-gray-100 h-screen overflow-hidden">
         <div class="pt-12">
-            <p class="text-center text-xl prose-sm">Upload your picture (preferranbly large) and we will generate you images for different breakpoints with optimal dimensions.</p>
+            <p class="text-center text-xl prose-sm">Upload your picture (preferably large) and we will generate you images for different breakpoints with optimal dimensions.</p>
         </div>
         <iframe src="https://bimbala-responsive-images.netlify.app" width="100%" height="100%" frameborder="0" loading="eager" class="overflow-hidden focus:outline-none" scrolling="no" fetchpriority="high">Your browser doesn't support iframes.</iframe>
     </div>
