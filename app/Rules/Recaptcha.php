b8c32260a3024d87ade1e75ff5d6c122238921e9
Lyuboslav Alexandrov <lyuboslav@bimbala.com>
2024-04-29 16:28:11 +0300

    Updated recaptcha rule

--- app/Rules/Recaptcha.php
+++ app/Rules/Recaptcha.php
@@ -10,10 +10,17 @@ class Recaptcha implements ValidationRule
     /**
      * Run the validation rule.
      *
-     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     * @param \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString $fail
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        //
+        $g_response = \Http::asForm()->post('https://www.google.com/recaptcha/api/siteverify', [
+            'secret' => config('recaptcha.api_secret_key'),
+            'response' => $value,
+            'remoteip' => request()?->getClientIp(),
+        ]);
+        if (!$g_response->json('success')) {
+            $fail("The {$attribute} is invalid.");
+        }
     }
 }
